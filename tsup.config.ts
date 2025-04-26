e9e9bf711310c2757262fb42de4b28bce5e56944
Justin Elmore <jelmore1674@pm.me>
2025-04-25 22:29:37 -0500

    update settings

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: [
-    "src/validations/index.ts",
-    "src/git/index.ts",
-  ],
+  outDir: "./dist",
+  entry: ["src/index.ts"],
   format: ["cjs", "esm"], // Build for commonJS and ESmodules
   dts: true, // Generate declaration file (.d.ts)
   splitting: true,
-  sourcemap: true,
   clean: true,
-  treeshake: true,
+  treeshake: "recommended",
+  minify: true,
 });
