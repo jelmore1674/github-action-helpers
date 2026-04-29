fd4213ca269da46718aab020b995908610e29a93
Justin Elmore <jelmore1674@pm.me>
2026-04-29 08:45:56 -0500

    fix bundle export

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,9 +3,12 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
   outDir: "./dist",
   entry: ["src/index.ts"],
-  format: ["cjs", "esm"], // Build for commonJS and ESmodules
-  dts: true, // Generate declaration file (.d.ts)
+  format: ["esm"],
+  dts: {
+    sourcemap: true,
+  }, // Generate declaration file (.d.ts)
   clean: true,
   minify: true,
   target: false,
+  fixedExtension: false,
 });
